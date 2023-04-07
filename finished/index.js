7ecda7222f1a0d4a29bd119078ca4e3a8972ef18
Salamander-m <moffatyapathy@gmail.com>
2023-04-08 02:27:34 +0300

    типа закончил задание

---
+++ finished/index.js
@@ -0,0 +1,41 @@
+const express = require('express'); // подключение модуля express
+
+const app = express(); // создание express app
+
+app.set("view engine", "ejs"); // подключение шаблонизатора ejs
+app.use(express.urlencoded({ extended: false })) // вызов доп ПО для обработки get запросов
+app.use(express.static('public')) // подключение папки, где находятся стили
+
+// обработка url'ov
+app.get("/", (req, res) => {
+    res.render("main")
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/user/:userName", (req, res) => {
+    const userData = { username: req.params.userName, hobbies: ["Футбол", "Игры", "Хуйня"] }
+    res.render("user", userData);
+});
+
+app.post("/yes", (req, res) => {
+    res.render("submitted")
+})
+
+app.post("/check-user", (req, res) => {
+    let userName = req.body.userName;
+    if (userName == "") {
+        return res.redirect("/");
+    } else {
+        return res.redirect("/user/" + userName);
+    }
+})
+
+//порт
+const PORT = 3000;
+//запуск сервера
+app.listen(PORT, () => {
+    console.log(`Server start is: http://127.0.0.1:${PORT}`)
+})
